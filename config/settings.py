fcc407f0f2c90bcfbc80b260fccf326f0c37f207
LERCIFU <lercifux6@gmail.com>
2026-01-15 20:57:56 +0700

    Setting Allow Hosts

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z+-^_*0=@ckkc4)m_omif2%^&(&0p_3^%djpacufir*k@+4!3o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
